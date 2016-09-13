655365324bc937c2aaf9e4ea31cafa71f3965a94
e <e@no.com>
2016-09-13 13:56:40 -0400

    removed header, slightly changed code style

--- sips.h
+++
@@ -1,9 +0,0 @@
-/* sips: a simple ips patcher */
-
-int check_header(FILE *patch);
-int read_records(FILE *patch);
-int apply_record(FILE *patch, uint16_t size, uint32_t offset);
-int apply_record_rle(FILE *patch, uint16_t size, uint32_t offset);
-static inline uint16_t byte2_to_uint(uint8_t *bytes);
-static inline uint32_t byte3_to_uint(uint8_t *bytes);
-int arghandler(int argc, char **argv, FILE **in, FILE **patch, FILE **out);
